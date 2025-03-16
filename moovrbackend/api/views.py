740bc57f7ebc91b5a489552acd35f0086e07c255
Ryan Deist <ryandeist@gmail.com>
2025-03-16 17:24:46 -0400

    created SignUpSerializer to handle new user creation. Removed some validation on passwords.

--- moovrbackend/api/views.py
+++ moovrbackend/api/views.py
@@ -8,7 +8,7 @@ from rest_framework.views import APIView
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.tokens import RefreshToken
 
-from .serializers import UserSerializer, JobSerializer
+from .serializers import SignupSerializer, JobSerializer
 from .models import Job
 
 # Create your views here.
@@ -34,10 +34,27 @@ class JobDeleteView(generics.DestroyAPIView):
         user = self.request.user
         return Job.objects.filter(user=user)
     
-class CreateUserView(generics.CreateAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-    permission_classes = [AllowAny]
+class SignupView(APIView):
+    def post(self, request):
+        if request.data['password'] != request.data['passwordConfirm']:
+            return Response({"error": "Passwords do not match."}, status=400)
+            
+        serializer =SignupSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            
+            return Response({
+                "message": "User created successfully.", 
+                "refresh": str(refresh),
+                "access": str(refresh.access_token),
+                "user": {
+                    "id": user.id,
+                    "username": user.username,
+                }
+            }, status=status.HTTP_201_CREATED)
+            
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class LoginView(APIView):
     def post(self, request):
--- moovrbackend/moovrbackend/urls.py
+++ moovrbackend/moovrbackend/urls.py
@@ -16,13 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from api.views import CreateUserView, LoginView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from api.views import SignupView, LoginView
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/user/signup/', CreateUserView.as_view(), name='signup'),
-    path('api/token/', LoginView.as_view(), name='get_token'),
+    path('api/user/signup/', SignupView.as_view(), name='signup'),
+    path('api/token/', LoginView.as_view(), name='login'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include('api.urls')),
