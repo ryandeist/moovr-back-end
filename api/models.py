de72e0c21be0cf7d679541690da06f83b2d00596
Ryan Deist <ryandeist@gmail.com>
2025-03-18 18:43:40 -0400

    Box Model added.

--- api/models.py
+++ api/models.py
@@ -2,6 +2,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+SIZES = (
+    ('1', 'Small'),
+    ('2', 'Medium'),
+    ('3', 'Large'),
+    ('4', 'Extra Large')
+)
+
 # Models
 class Job(models.Model):
     customer_name = models.CharField(max_length=100)
@@ -14,3 +21,18 @@ class Job(models.Model):
     
     def __str__(self):
         return self.customer_name
+
+class Box(models.Model):
+    box_name = models.CharField(max_length=100)
+    size = models.CharField(
+        max_length=1,
+        choices=SIZES,
+        default=SIZES[0][0]
+        )
+    box_full = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    job = models.ForeignKey(Job, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.box_name
